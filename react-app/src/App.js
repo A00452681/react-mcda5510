a59986913fd484b954f2935e1bf44d06ed078f43
Simon Achkar <simon@simonachkar.com>
2021-03-25 01:33:58 -0300

    Added simple component (Province.js)

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Province from './components/Province';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <img alt="Canada's Flag" width={125} src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg" />
+      <h2>Canada's Provinces</h2>
+      <Province name="Nova Scotia" flagUrl="https://upload.wikimedia.org/wikipedia/commons/c/c0/Flag_of_Nova_Scotia.svg" />
     </div>
   );
 }
---
+++ react-app/src/components/Province.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react"
+import './styles.css';
+
+class Province extends Component {
+
+    render() {
+        return (
+            <div className="province-container">
+                <img alt={`${this.props.name}'s Flag`} width={125} src={this.props.flagUrl} />
+                <h3>{this.props.name}</h3>
+                <button>Show Capital</button>
+            </div>
+        )
+    }
+}
+
+export default Province;
+
+/**
+ * Topics you might also like:
+ *      - Object Destructuring -> https://dmitripavlutin.com/javascript-object-destructuring/
+ *      - Template Literals -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
+ *      - Rendering Elements -> https://reactjs.org/docs/rendering-elements.html
+ */
