c7c1b1cc69a25a8a1ca03fc32d01e0a2ef7e1647
Simon Achkar <simon@simonachkar.com>
2021-03-25 02:27:54 -0300

    added menu

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,7 +6,12 @@ function App() {
     <div className="App">
       <h1>Hello World</h1>
       <img alt="Canada's Flag" width={125} src="https://upload.wikimedia.org/wikipedia/commons/d/d9/Flag_of_Canada_%28Pantone%29.svg" />
-      <h2>Canada's Provinces</h2>
+
+      <div className="menu">
+        <p className="menu-item">Provinces</p>
+        <p className="menu-item">Territories</p>
+      </div>
+
       <Provinces />
     </div>
   );
