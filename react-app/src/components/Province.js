4d071457d7e497ac4400a3b01027e613b99774c6
Simon Achkar <simon@simonachkar.com>
2021-03-25 01:46:07 -0300

    added state + onClick event

--- react-app/src/components/Province.js
+++ react-app/src/components/Province.js
@@ -2,13 +2,22 @@ import React, { Component } from "react"
 import './styles.css';
 
 class Province extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isShowCapital: false
+        }
+    }
 
     render() {
         return (
             <div className="province-container">
                 <img alt={`${this.props.name}'s Flag`} width={125} src={this.props.flagUrl} />
-                <h3>{this.props.name}</h3>
-                <button>Show Capital</button>
+                <div>
+                    <h3>{this.props.name}</h3>
+                    {this.state.isShowCapital ? <p>Halifax</p> : undefined}
+                </div>
+                <button onClick={() => this.setState({ isShowCapital: !this.state.isShowCapital })}>Show Capital</button>
             </div>
         )
     }
