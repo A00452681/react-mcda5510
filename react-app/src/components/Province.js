da800dcc09c8038512b1c8ae10da7571d7b6ec6a
Simon Achkar <simon@simonachkar.com>
2021-03-27 00:02:45 -0300

    updated states and events code

--- react-app/src/components/Province.js
+++ react-app/src/components/Province.js
@@ -1,25 +1,27 @@
 import React, { Component } from "react"
-import './styles.css';
 
 class Province extends Component {
     constructor(props) {
         super(props)
+
         this.state = {
-            isShowCapital: false
+            capital: ''
         }
+
+        this.click = this.click.bind(this)
+    }
+
+    click() {
+        this.setState({ capital: this.props.capital })
     }
 
     render() {
+        console.log('state is ' + this.state.capital)
         return (
             <div className="province-container">
                 <img alt={`${this.props.name}'s Flag`} width={125} src={this.props.flagUrl} />
-                <div>
-                    <h3>{this.props.name}</h3>
-                    {this.state.isShowCapital ? <p>{this.props.capital}</p> : undefined}
-                </div>
-                <button onClick={() => this.setState({ isShowCapital: !this.state.isShowCapital })}>
-                    {this.state.isShowCapital ? "Hide Capital" : "Show Capital"}
-                </button>
+                <h3>{this.props.name}</h3>
+                <button className="btn-capital" onClick={() => this.setState({ capital: this.props.capital })}>Show Capital</button>
             </div>
         )
     }
@@ -32,4 +34,5 @@ export default Province;
  *      - Object Destructuring -> https://dmitripavlutin.com/javascript-object-destructuring/
  *      - Template Literals -> https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  *      - Rendering Elements -> https://reactjs.org/docs/rendering-elements.html
+ *      - Handling events -> https://reactjs.org/docs/handling-events.html
  */
